97fe9a42da0cab9f88afef17be8b6d09fd518c0f
aaldhamari2346 <126210747+aaldhamari2346@users.noreply.github.com>
2023-02-27 13:19:12 -0700

    Closed loop system and Open loop system
    These codes open a Simulink file which.  models a closed loop and open loop system of our robot's. velocity and position.

---
+++ MiniProject/MATLAB-Simulink Code/miniproject47openloop.m
@@ -0,0 +1,12 @@
+% SEED Lab EENG350
+%mini project open loop system
+%% miniproject47.m
+%% Transferfunction Values
+K=0.00375;
+a=20;
+%% Run a Simulation
+open_system('MiniprojectOL')
+out=sim('MiniprojectOL');
+%% A Plot of the results
+figure
+plot(out.simout,'b:')
