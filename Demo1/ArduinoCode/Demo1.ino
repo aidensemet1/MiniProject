9b961122006abb0f7c1a932fc07653cf9c13fc12
aidensemet1 <125506571+aidensemet1@users.noreply.github.com>
2023-03-06 13:32:06 -0700

    Update Demo1.ino

--- Demo1/ArduinoCode/Demo1.ino
+++ Demo1/ArduinoCode/Demo1.ino
@@ -2,7 +2,9 @@ unsigned long Tc; //current time =
 int N = 1; // loop iterations
 #include <Encoder.h>   // encoder Arduino library
 
+//distance and angle
 float desiredDistance = 10; //in feet
+float requiredRadians; //calculated radians required to turn based on desired distance to travel
 float desiredAngle = 90; //in degrees
 
 //encoder pins for localization (2 and 3 are interrupt pins)
@@ -10,11 +12,14 @@ Encoder Lwheel(2, 4);
 Encoder Rwheel(3, 5);
 
 String read_setpoint;
-float ref;  //reference position for wheel position
+float ref;  //radians for the wheel to turn
 float theta = 0;
 long count = 0;
 long newCount;
 float radPerCount = 0.001963;
+float pi = 3.14159;
+float diameter = 5.5;
+float circumference = pi * diameter;
 
 //SETUP PINS (don't change these, they are specified on the motor driver data sheet)
 int D2 = 4; //motor board enable pin
@@ -39,33 +44,16 @@ float Ts = 5; // sampling rate I think?(10 ms)
   // put your setup code here, to run once:
   Serial.begin(250000);  //start serial communication w/ baud rate of 250000
   motorSetup();
+  
+  //distance calculations
+  requiredRadians = (desiredDistance/(circumference/2))*pi; //radians required to travel desired distance
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    read_setpoint = Serial.readString();  //get data sent from Pi
-    //Serial.println(read_setpoint);
-    if (read_setpoint == "0") {  //if received setpoint = 0
-      ref = 0;                   //set ref position = 0
-      Serial.print(ref);         //print to serial monitor
-    }
-    else if (read_setpoint == "pi/2") {   //if received setpoint = pi/2
-      ref = 1.57;                         //set ref position = pi/2 = 1.57
-      Serial.print(ref);                  //print to serial monitor
-    }
-    else if (read_setpoint == "pi") {    //if received setpoint = pi
-      ref = 3.14;                        //set ref position = pi = 3.14
-      Serial.print(ref);                 //print to serial monitor
-    }
-    else if (read_setpoint == "3pi/2") {   //if received setpoint = 3pi/2
-      ref = 4.71;                          //set red position = 3pi/2 = 4.71
-      Serial.print(ref);                   //print to serial monitor
-    }
-  }
-  
+
   Tc = millis(); //get current time ms
-  e = ref - getCurrentPos();
+  e = requiredRadians - getCurrentPos();
   Ie = Ie + e * Ts*0.001; // calculating the integral
   int C = Kp* e + Ie* Ki; // This will be in volts
 
