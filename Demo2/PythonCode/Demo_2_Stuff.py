c83f55de82baee043762c712a65dbb3b8eeb2c04
kelvinh1 <125506341+kelvinh1@users.noreply.github.com>
2023-04-07 12:53:51 -0600

    Update Demo_2_Stuff.py

--- Demo2/PythonCode/Demo_2_Stuff.py
+++ Demo2/PythonCode/Demo_2_Stuff.py
@@ -4,16 +4,17 @@ from picamera import PiCamera
 from time import sleep
 import serial
 import smbus
+import struct
 import time
 import board
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
 # Initialize LCD display
-lcd_columns = 16
-lcd_rows = 2
-i2c = board.I2C()
-lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
-lcd.color = [100,0,0]
+#lcd_columns = 16
+#lcd_rows = 2
+#i2c = board.I2C()
+#lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+#lcd.color = [100,0,0]
 
 #camera = PiCamera()
 '''
@@ -29,20 +30,12 @@ w = 1280
 h = 840
 '''
 
-'''
-# Setup serial communication
-arduino = serial.Serial("/dev/ttyACM0", 250000, timeout=2)
+address = 0x04
 bus = smbus.SMBus(1)
-'''
-
-# Initialize LCD display
-lcd_columns = 16
-lcd_rows = 2
-i2c = board.I2C()
-lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
 
 def continualCapture():
     loopNum = 1
+    
     print("Starting calibration")
     cap = cv.VideoCapture(0)
     cap.set(3, w) # set horizontal resolution
@@ -71,32 +64,94 @@ def continualCapture():
 
         if ids is not None:
             
-            #arduino.write(str("1").encode())
-            lcd.clear()
-            lcd.message = "Marker detected"
+            bus.write_byte_data(address, 0, 1)
+            #lcd.clear()
+            #lcd.message = "Marker detected"
             break;
         
-        cv.imshow('Detect Image', frame)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+        #cv.imshow('Detect Image', frame)
+        #if cv.waitKey(1) & 0xFF == ord('q'):
+        #    break
     
     # end while
     cap.release()
-    cv.destroyAllWindows()
+    #cv.destroyAllWindows()
     
     print("Ending detection...")
 
-def distanceFinder():
+def angleFinder():
     
-    print("Starting calibration")
+    #print("Starting calibration")
     cap = cv.VideoCapture(0)
     cap.set(3, w) # set horizontal resolution
     cap.set(4, h) # set vertical resolution
     cap.set(5, 10)
     cap.set(cv.CAP_PROP_EXPOSURE, 1)
     sleep(2)
-    lcd.clear()
-    print("Ending calibration")
+    #lcd.clear()
+    #print("Ending calibration")
+    
+    print("Starting angle...")
+    # Start video capture stream and initilize aruco dictionary
+    dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
+    
+    parameters = cv.aruco.DetectorParameters_create()
+    angle = 0.0
+    
+    
+    while(True):
+        ret, frame = cap.read()
+        
+        gray = frame
+
+        corners, ids, rejectedImgPoints = cv.aruco.detectMarkers(gray, dictionary, parameters = parameters)
+        cv.aruco.detectMarkers(gray, dictionary, corners, ids)
+        cv.line(gray, (int(w/2),0), (int(w/2),h), (0,255,0),3)
+
+        if ids is not None:
+            
+            first = corners[0][0][0]
+            third = corners[0][0][2]
+                
+            middle_x = 0.5*(third[0] + first[0]) 
+            middle_y = 0.5*(third[1] + first[1])
+            
+            
+            center = 0.5 * w
+        
+            angle = (27 * ( (center - middle_x) / center)) + 4
+            print("Angle * 100: %s" % angle)
+            fudge_angle = (angle + 54)
+            
+            send_angle = int(fudge_angle)
+            bus.write_byte_data(address, 1, send_angle)
+            #lcd.clear()
+            #lcd.message = "Fixing Angle"
+            break
+        
+        
+        #cv.imshow('Detect Image', frame)
+        #if cv.waitKey(1) & 0xFF == ord('q'):
+         #   break
+    
+    # end while
+    cap.release()
+    #cv.destroyAllWindows()
+    
+    print("Ending angle...")
+    
+
+def distanceFinder():
+    
+    #print("Starting calibration")
+    cap = cv.VideoCapture(0)
+    cap.set(3, w) # set horizontal resolution
+    cap.set(4, h) # set vertical resolution
+    cap.set(5, 10)
+    cap.set(cv.CAP_PROP_EXPOSURE, 1)
+    sleep(2)
+    #lcd.clear()
+    #print("Ending calibration")
     
     print("Starting distance...")
     # Start video capture stream and initilize aruco dictionary
@@ -128,9 +183,23 @@ def distanceFinder():
             middle_y = 0.5*(third[1] + first[1])
             
             v = third[1] - first[1] #camera coord image height in pixels
-            distance = 2849.3 * (1/v) + 5.3856
+            
+            #normal distance
+            #distance = (2849.3 * (1/v) + 5.3856)-6.20
+            
+            #stop within 1 ft of marker
+            distance = (2849.3 * (1/v) + 5.3856)-28
+            
+            send_dist = int(distance)
+            bus.write_byte_data(address, 2, send_dist)
+            #lcd.clear()
+            #lcd.message = "Going Dist"
+            
+            #lcd.clear()
+            #lcd.message = "D:" + str(distance)
             #print("v:\t%3.5f" % v)
             print(distance)
+            break
             
             '''
             center = 0.5 * w
@@ -146,19 +215,17 @@ def distanceFinder():
             cv.line(gray, (int(w/2), middle_y), (middle_x,middle_y),(0,0,255),3)
             '''
         
-        
-        
-        cv.imshow('Detect Image', frame)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+        #cv.imshow('Detect Image', frame)
+        #if cv.waitKey(1) & 0xFF == ord('q'):
+        #    break
     
     # end while
     cap.release()
-    cv.destroyAllWindows()
+    #cv.destroyAllWindows()
     
     print("Ending distance...")
     
         
-#continualCapture()
+continualCapture()
+angleFinder()
 distanceFinder()
-  
